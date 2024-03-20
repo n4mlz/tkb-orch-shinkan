e4aa209bd70c70111bd7c2e37f65b57b1af454dc
Nameless-itf23 <ohutoncat@gmail.com>
2024-03-21 03:56:11 +0900

    feat: styles for top-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,11 +13,11 @@ import heroImage from "@images/hero.jpg";
 
 const TransitionButton = (props: { link: string; text: string }) => {
   return (
-    <div className={styles.transitionButton}>
-      <Link href={props.link}>
+    <Link href={props.link}>
+      <div className={styles.transitionButton}>
         <p className={styles.transitionButtonText}>{props.text}</p>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 };
 
@@ -44,8 +44,8 @@ const Home = () => {
           <p className={styles.sectionTitle}>直近の新歓イベント</p>
           <table className={styles.eventListContent}>
             <tbody>
-              {eventList.map((welcomeEvent) => (
-                <tr>
+              {eventList.map((welcomeEvent, index) => (
+                <tr key={index}>
                   <td>{getFormattedDate(welcomeEvent.date)}</td>
                   <td>{welcomeEvent.name}</td>
                 </tr>
@@ -53,61 +53,76 @@ const Home = () => {
             </tbody>
           </table>
         </div>
-        <div className={styles.transitionButtonWrapper}>
+        <div className={styles.welcomeEventButtonWrapper}>
           <TransitionButton link="/event" text="新歓情報" />
         </div>
       </div>
       <div className={styles.about}>
-        <p className={styles.sectionTitle}>つくオケとは</p>
+        <p className={styles.sectionTitleWhite}>つくオケとは</p>
         <p className={styles.aboutContent}>
-          筑波大学管弦楽団は1974年に創団された、筑波大学内では最大規模を誇る課外活動団体の1つです。練習、演奏会やその他の活動を通して、団の演奏技術の向上、そして団員間の親睦を深めることを目的としています。
+          筑波大学管弦楽団は1974年に創団された、
+          <br />
+          筑波大学内では最大規模を誇る課外活動団体の1つです。
+          <br />
+          練習、演奏会やその他の活動を通して、団の演奏技術の向上、
+          <br />
+          そして団員間の親睦を深めることを目的としています。
         </p>
         <TransitionButton link="/about" text="団体について" />
       </div>
       <div className={styles.introduction}>
-        <div className={styles.introductionBlock}>
-          <p className={styles.introductionTitle}>セクション紹介</p>
+        <div className={styles.sectionIntroduction}>
+          <p className={styles.sectionTitleWhite}>セクション紹介</p>
           <TransitionButton link="/introduction" text="詳細を見る" />
         </div>
-        <div className={styles.introductionBlock}>
-          <p className={styles.introductionTitle}>パート紹介</p>
+        <div className={styles.partIntroduction}>
+          <p className={styles.sectionTitleWhite}>パート紹介</p>
           <TransitionButton link="/introduction" text="詳細を見る" />
         </div>
       </div>
       <div className={styles.interview}>
-        <div className={styles.sectionTitle}>団員インタビュー</div>
-        <div className={styles.interviewDescription}>
-          大学から新しく楽器を始めた人や、医学類の人、複数のサークルに所属している人など、様々な境遇の団員にインタビューをしました。
+        <div className={styles.interviewSummary}>
+          <div className={styles.sectionTitle}>団員インタビュー</div>
+          <div className={styles.interviewDescription}>
+            大学から新しく楽器を始めた人や、医学類の人、複数のサークルに所属している人など、様々な境遇の団員にインタビューをしました。
+          </div>
         </div>
         <div className={styles.sliderContainer}>
-          <Splide
-            options={{
-              autoplay: true,
-              interval: 3000,
-              perPage: 3,
-              perMove: 1,
-            }}>
-            {interviewThumbList.map((thumb) => (
-              <SplideSlide>
-                {/* TODO: ここをリンクに対応する */}
-                <Image className={styles.interviewThumb} src={thumb} alt="団員インタビューのサムネイル" />
-              </SplideSlide>
-            ))}
-          </Splide>
+          <div className={styles.slider}>
+            <Splide
+              options={{
+                autoplay: true,
+                interval: 3000,
+                rewind: true,
+                perPage: 3,
+                perMove: 1,
+              }}>
+              {interviewThumbList.map((thumb, index) => (
+                <SplideSlide key={index}>
+                  {/* TODO: ここをリンクに対応する */}
+                  <Image className={styles.interviewThumb} src={thumb} alt="団員インタビューのサムネイル" />
+                </SplideSlide>
+              ))}
+            </Splide>
+          </div>
         </div>
         <TransitionButton link="/interview" text="インタビュー一覧" />
       </div>
       <div className={styles.moreInfo}>
         <div className={styles.moreInfoBlock}>
-          <p className={styles.sectionTitle}>Q&A</p>
-          <p className={styles.moreInfoDescription}>
-            新入生が疑問に思いそうなことについてQ&A形式で一覧にしています。SNSで来た質問も随時こちらに掲載します。
-          </p>
+          <div className={styles.moreInfoSummary}>
+            <p className={styles.sectionTitle}>Q&A</p>
+            <p className={styles.moreInfoDescription}>
+              新入生が疑問に思いそうなことについてQ&A形式で一覧にしています。SNSで来た質問も随時こちらに掲載します。
+            </p>
+          </div>
           <TransitionButton link="/faq" text="一覧を見る" />
         </div>
         <div className={styles.moreInfoBlock}>
-          <p className={styles.sectionTitle}>入団方法</p>
-          <p className={styles.moreInfoDescription}>入団方法はこちらからご覧になれます。</p>
+          <div className={styles.moreInfoSummary}>
+            <p className={styles.sectionTitle}>入団方法</p>
+            <p className={styles.moreInfoDescription}>入団方法はこちらからご覧になれます。</p>
+          </div>
           <TransitionButton link="/joinus" text="詳細を見る" />
         </div>
       </div>
