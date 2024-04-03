28f00912fad1282eabbd7dd60934b08ac60f7d64
Nameless-itf23 <ohutoncat@gmail.com>
2024-04-03 22:45:09 +0900

    feat: add section and part information to Introduction page

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -10,13 +10,13 @@ import thumb8 from "@images/interview/thumb8.png";
 import thumb9 from "@images/interview/thumb9.png";
 
 import strings from "@images/section/strings.png";
-import woodwind from "@images/section/woodwind.png";
+import woodwinds from "@images/section/woodwinds.png";
 import brass from "@images/section/brass.png";
 
 import violin from "@images/part/violin.png";
 import viola from "@images/part/viola.png";
 import cello from "@images/part/cello.png";
-import bass from "@images/part/bass.png";
+import doubleBass from "@images/part/doubleBass.png";
 import flute from "@images/part/flute.png";
 import oboe from "@images/part/oboe.png";
 import clarinet from "@images/part/clarinet.png";
@@ -24,14 +24,13 @@ import bassoon from "@images/part/bassoon.png";
 import horn from "@images/part/horn.png";
 import trumpet from "@images/part/trumpet.png";
 import trombone from "@images/part/trombone.png";
-import tuba from "@images/part/tuba.png";
 import percussion from "@images/part/percussion.png";
 
 const interviewThumbList = [thumb0, thumb1, thumb2, thumb3, thumb4, thumb5, thumb6, thumb7, thumb8, thumb9];
 
 const sectionImages = {
   strings,
-  woodwind,
+  woodwinds,
   brass,
 };
 
@@ -39,7 +38,7 @@ const partImages = {
   violin,
   viola,
   cello,
-  bass,
+  doubleBass,
   flute,
   oboe,
   clarinet,
@@ -47,7 +46,6 @@ const partImages = {
   horn,
   trumpet,
   trombone,
-  tuba,
   percussion,
 };
 
